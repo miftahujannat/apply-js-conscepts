f72be7a5a3b333eda8a86b21bc0a7e374458d9c1
miftahujannat <miftahurimny@gmail.com>
2024-05-07 20:10:39 +0600

    added

---
+++ inchtofeet.js
@@ -0,0 +1,23 @@
+function inchtofeet(inches){
+    var feet = inches / 12;
+    return feet;
+}
+
+
+// var myinches = 132;
+// var feet = inchtofeet(myinches);
+// console.log('myinches is feet',feet);
+
+// var dadinches = 144;
+// var feet = inchtofeet(dadinches);
+// console.log(feet);
+
+
+// mile to km 
+function miletokilomiter(miles){
+    var km = miles * 1.60934;
+    return km; 
+}
+var marathon = miletokilomiter(26.2);
+console.log(marathon);
+
---
+++ recap.js
@@ -0,0 +1,53 @@
+var time = '10:15pm';
+var bookprice = 150;
+var iswhitecolor = false;
+
+// array 
+var partners = ['sahd','mojid', 'nojid','lojid'];
+var elementcount = partners.length;
+var nojidindex = partners.indexOf('nojid');
+partners.push('kajil');
+partners.pop();
+
+// conditions 
+if(bookprice < 128){
+    // console.log('I will buy thid book');
+}
+else{
+    // console.log('mama kisu discount den ')
+}
+
+var i = 0;
+while(i <= 17){
+    // do some work 
+    i++;
+} 
+
+for(var i = 0; i <= 17; i++){
+
+}
+
+// function 
+function ismoonup(time){
+    if(time >= 19 && time <= 5){
+        return true;
+    }
+    return false;
+}
+var moonstar = ismoonup(21);
+// console.log(moonstar);
+
+// let const 
+var price = 27;
+price = 29;
+price = 29;
+
+var name = 'lal e lal helal';
+
+// value of the variable will not change 
+const myname = 'lal ee lal helal';
+console.log(myname);
+myname = 22;
+
+
+
