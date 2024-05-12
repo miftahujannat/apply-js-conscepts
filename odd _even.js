3c7eef03cc77cc903d506f3b14221cb6da12e6a6
miftahujannat <miftahurimny@gmail.com>
2024-05-12 19:13:29 +0600

    added

---
+++ odd _even.js
@@ -0,0 +1,15 @@
+// const number = 4;
+// const remainder = number % 2;
+// console.log(remainder == 0);
+
+const number = 145;
+function isEven(number){
+    if(number % 2 == 0){
+        return true;
+    }
+    return false
+
+}
+
+const mynumber = 1641;
+const ismynumbereven = isEven(mynumber);
