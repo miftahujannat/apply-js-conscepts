f837eb4af7bda001ae0767d58fb1bc9ac913ebbe
miftahujannat <miftahurimny@gmail.com>
2024-05-13 17:29:41 +0600

    added

---
+++ exmple_factorial.js
@@ -0,0 +1,37 @@
+// var factorial = 5;
+// for (i = 1; i <= 5; i++){
+//     // console.log(i);
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+// function getfactorial(number){
+//     let factorial = 1;
+//     for(i = 1; i <= number; i++){
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// var firstfactorial = getfactorial(5);
+// console.log(firstfactorial); 
+
+// function getfactorial(number){
+//     let factorial = 1;
+//     for(i = 1; i <= number; i++){
+        
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+// var firstfactorial = getfactorial(5);
+// console.log(firstfactorial); 
+
+var factorial = 1;
+for (i = 1; i <= 1000; i++){
+    console.log(i);
+   
+    factorial = factorial * i;
+}
+console.log(factorial);
+
+
