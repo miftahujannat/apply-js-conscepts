1284bc16e00748f1a90041d12eb7a6315d1095d8
miftahujannat <miftahurimny@gmail.com>
2024-05-16 10:05:41 +0600

    added

--- leap_year.js
+++ leap_year.js
@@ -1,18 +1,44 @@
-function isleapyear(year){
-    if(year % 4 == 0){
+// function isleapyear(year){
+//     if(year % 4 == 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+
+// }
+
+// const myyear = 2087;
+// const ismyleapyear = isleapyear(myyear);
+// console.log('Is my year leap year',ismyleapyear);
+
+
+// const youryear = 2224;
+// const isyourleapyear = isleapyear(youryear);
+// console.log('is your leap year',isyourleapyear);
+
+// leap year
+
+// function isleapyear(year){
+//     if ( year % 4 == 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const myyear  = 2223;
+// const ismyleapyear = isleapyear(myyear);
+// console.log(ismyleapyear);
+
+function  isleapyear(year){
+    if (year % 4 == 0){
         return true;
     }
     else{
         return false;
     }
-
 }
-
-const myyear = 2087;
-const ismyleapyear = isleapyear(myyear);
-console.log('Is my year leap year',ismyleapyear);
-
-
-const youryear = 2224;
-const isyourleapyear = isleapyear(youryear);
-console.log('is your leap year',isyourleapyear);
+const myyear = 2228;
+const isyourleapyear = isleapyear(myyear);
+console.log(isyourleapyear);
